dcaca5bcc21ea673f145b949271a1ad26ba82c5a
David <dmc-source@dcorking.com>
2012-12-07 16:50:53 +0000

    basic scaffold and Test::Unit tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dreamblog::Application.config.secret_token = '3c33196ad5935f7e7d3662a7a932ff8ae3ef76358d72773850497b7ed370c19645b39240cb8549801fbe714d3e6505ce24d9b6cea14b763281910ee835ca84a5'
